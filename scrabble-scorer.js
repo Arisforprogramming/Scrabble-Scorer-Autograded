ab2abe950234c487da7db70416fb7f0716555b67
arissaracode <133154320+Arisforprogramming@users.noreply.github.com>
2024-07-19 00:32:48 -0500

    add new code

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -31,79 +31,96 @@ function oldScrabbleScorer(word) {
 
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
-//Modify the provided initialPrompt() function to prompt the user to enter a word to score.
-function initialPrompt() {
-   console.log("Let's play some scrabble!");
-   let userInput = input.question("\nEnter a word to score: "); 
-   /*console.log(`Which scoring algorithm would you like to use?
 
+function initialPrompt(questions) {
+   console.log("Let's play some scrabble!\n\n");
+   let askQuestion = input.question("Enter a word to score: ");
+
+   return askQuestion;
+};
+
+let question2 =  
+  `Which scoring algorithm would you like to use?
+  
    0 - Simple: One point per character
    1 - Vowel Bonus: Vowels are worth 3 points
-   2 - Scrabble: Uses scrabble point system`)*/
-   console.log(oldScrabbleScorer(userInput))
-
-   return userInput;
-};
+   2 - Scrabble: Uses scrabble point system
+   Enter 0, 1, or 2: `
 
 let newPointStructure;
 
-let simpleScorer = function(word){
-
-    let total = 0;
-        word =  word.toUpperCase();
-     for(i= 0; i < word.length; i++){
-         total += 1;
-     }
-     return  total;
-};  
-   /* this console id just to test simpleScorer function
-    console.log(simpleScorer("banana")) */
-
- let vowelBonusScorer = function(word){
-    let total = 0;
-        word = word.toUpperCase();
-    const vowels = "aeiou"
-      for(let i=0; i< word.length; i+3){
-         if( vowels.includes(word)[i]) {
-               total +=3
-         }else {
-               total +=1
-         }
-         return total;
-      }
+let simpleScorer = function(word1){
+   word1 = word1.toUpperCase();
+  let letterPoints = "";
+  for (let i = 0; i < word1.length; i++) {
+    letterPoints += `Points for '${word1[i]}': 1\n`
+  }
+  return letterPoints;
 };
-// when i add console.log here it stop the program from runing 
-let scrabbleScorer //= oldScrabbleScorer;
 
-const scoringAlgorithms = [] /*= [ 
-   {  
-      name : "Simple Score",
-      Description : "Each letter is worth 1 point.",
-      ScoreFunction : simpleScorer
-   },
-   {
-      name: "Bonus Vowels", 
-      description: "Vowels are 3 pts, consonants are 1 pt.",
-      scoringFunction : vowelBonusScorer
-   },
-   {
-      name: "Scrabble", 
-      description: "The traditional scoring algorithm.",
-      scoringFunction : scrabbleScorer
-   }
+let vowelBonusScorer = function(word2) {
+    word2 = word2.toUpperCase();
+  let letterPoints = "";
+  for(let i = 0; i < word2.length; i++) {
+    if(word2[i] === "A" || word2[i] === "E" || word2[i] === "I" || word2[i] === "O" || word2[i] === "U") {
+      letterPoints += `Points for '${word2[i]}': 3\n`
+    } else {
+      letterPoints += `Points for '${word2[i]}': 1\n`
+    }
+  }
+   return letterPoints;
+};
 
-   ]; */
+let scrabbleScorer = oldScrabbleScorer;
 
-function scorerPrompt() {
-   
+const scoringAlgorithms = 
+  [ 
+    {
+    name : "simple Score" ,
+    description: "Each letter is worth 1 point." ,
+    scoreFunction : simpleScorer
+    },
+    {
+     name : "Bonus Vowels" ,
+     description: "Vowels are 3 pts, consonants are 1 pt." ,
+     scoreFunction : vowelBonusScorer
+     },
+     {
+      name : "Scrabble" ,
+      description: "Uses the oldScrabbleScorer() function to determine the score for a given word." ,
+       scoreFunction : scrabbleScorer
+     }
+  ];
 
-
-}
+ function scorerPrompt (userSelect) {
+     userSelect = Number(input.question(question2));
+  if (userSelect === 0){
+    console.log(`\nYou have selected the simple scoring algorithm.\n\n${simpleScorer()}`)
+  } else if(userSelect === 1) {
+    console.log(`\nYou have selected the Bonus Vowels algorithm.\n\n${vowelBonusScorer()}`)
+  } else if(userSelect === 2) {
+     console.log(`\nYou have selected the Scrabblealgorithm.\n\n${scrabbleScorer()}`)
+  } else {
+    (console.log("Please only select 0,1 or 2"))
+  }
+  return userSelect;
+} 
 
 function transform() {};
 
 function runProgram() {
-   initialPrompt();
+  let wordInput = initialPrompt();
+  let userSelectNum = scorerPrompt();
+  console.log(userSelectNum);
+  // let calculate1 = oldScrabbleScorer(wordInput);
+  // console.log(calculate1);
+  let calculate1 = scorerPrompt(wordInput);
+  console.log(calculate1);
+  // this line test for simplesScorer
+  //let simpleScorerTest = console.log(simpleScorer("BANANA"));
+  // this line test for vowelesScorer
+  // let vowelBonusScorerTest = console.log(vowelBonusScorer("Elephant"));
+  // console.log(vowelBonusScorerTest);
 }
 
 // Don't write any code below this line //
